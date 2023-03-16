467c734dea795342b4723085a18767ee4b91db34
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2023-03-16 08:57:27 -0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ function App() {
         <Information />
         <div className='projects'>
           <Project />
-          <Project />
-          <Project />
         </div>
       </div>
     </div>
