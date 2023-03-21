0e466f5153deb057fab5f7f40325d5a1d6349bab
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2023-03-20 22:26:51 -0700

    parllax changes

--- src/components/ParallaxBackground.jsx
+++ src/components/ParallaxBackground.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import styles from './ParallaxBackground.module.css'
 import { Parallax } from 'react-scroll-parallax';
 
 export default function ParallaxBackground() {
+  const windowSize = useRef([window.innerWidth, window.innerHeight]);
+  const [isActive, setIsActive] = useState(true)
+  
+  useEffect(() => {
+    if (windowSize.current[0] <= 768) {
+      console.log("Less than 768")
+      setIsActive(false)
+    }
+  }, [])
+
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <span className={styles.name}>Bryan Chhorb</span>
         <span className={styles.position}>Full Stack Developer</span>
       </div>
-      <Parallax easeing={"linear"} speed={-3} className={`${styles["bg-1"]} ${styles.all}`} />
-      <Parallax easeing={"linear"} speed={-40} className={`${styles["bg-2"]} ${styles.all}`} />
-      <Parallax easeing={"linear"} speed={-50} className={`${styles["bg-3"]} ${styles.all}`} />
-      <Parallax easeing={"linear"} speed={-60} className={`${styles["bg-4"]} ${styles.all}`} />
-      <Parallax easeing={"linear"} speed={-70} className={`${styles["bg-5"]} ${styles.all}`} />
-      <Parallax easeing={"linear"} speed={-70} className={`${styles["bg-6"]} ${styles.all}`} />
-      <Parallax easeing={"linear"} speed={-80} className={`${styles["bg-7"]} ${styles.all}`} />
-      <Parallax easeing={"linear"} speed={-80} className={`${styles["bg-8"]} ${styles.all}`} />
-      <Parallax easeing={"linear"} speed={-80} className={`${styles["bg-gradient"]} ${styles.all}`} />
+      <Parallax easeing={"linear"} speed={isActive ? -2 : 0} className={`${styles["bg-1"]} ${styles.all}`} />
+      <Parallax easeing={"linear"} speed={isActive ? -20 : 0} className={`${styles["bg-2"]} ${styles.all}`} />
+      <Parallax easeing={"linear"} speed={isActive ? -30 : 0} className={`${styles["bg-3"]} ${styles.all}`} />
+      <Parallax easeing={"linear"} speed={isActive ? -40 : 0} className={`${styles["bg-4"]} ${styles.all}`} />
+      <Parallax easeing={"linear"} speed={isActive ? -50 : 0} className={`${styles["bg-5"]} ${styles.all}`} />
+      <Parallax easeing={"linear"} speed={isActive ? -50 : 0} className={`${styles["bg-6"]} ${styles.all}`} />
+      <Parallax easeing={"linear"} speed={isActive ? -60 : 0} className={`${styles["bg-7"]} ${styles.all}`} />
+      <Parallax easeing={"linear"} speed={isActive ? -60 : 0} className={`${styles["bg-8"]} ${styles.all}`} />
+      <Parallax easeing={"linear"} speed={isActive ? -60 : 0} className={`${styles["bg-gradient"]} ${styles.all}`} />
     </div>
   )
 }
